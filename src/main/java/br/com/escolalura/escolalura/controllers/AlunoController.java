ed873cb50d5f6d85aa90a3be373779d76aa962ad
Bruno Vieira Luz <brunov14@gmail.com>
2018-01-06 20:04:02 -0300

    Vizualizar os dados dos alunos

--- src/main/java/br/com/escolalura/escolalura/controllers/AlunoController.java
+++ src/main/java/br/com/escolalura/escolalura/controllers/AlunoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.escolalura.escolalura.models.Aluno;
@@ -38,7 +39,14 @@ public class AlunoController {
 		return "aluno/listar";
 	}
 	
-	
+	@GetMapping("/aluno/visualizar/{id}")
+	public String visualizar(@PathVariable String id, Model model){
+		Aluno aluno = repositorio.obterAlunoPor(id);
+		
+		model.addAttribute("aluno", aluno);
+		
+		return "aluno/visualizar";
+	}
 	
 	
 }
